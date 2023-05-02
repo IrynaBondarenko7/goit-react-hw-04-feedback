4c18d0bf2a8ed3e876064cb2357ea126f054286b
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-05-02 18:41:18 +0100

    changed class App for hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,75 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Layout } from './Layout/Layout';
+import { Section } from './Section/Section';
+import { Statistic } from './Statistic/Statistic';
+import { Notification } from './Notification/Notification';
+
 export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const updateStatistic = value => {
+    switch (value) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
+  };
+
+  const countTotalFeedback = () => {
+    let total = good + bad + neutral;
+    return total;
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const totalValue = countTotalFeedback();
+    let positiveValaue = Math.round((good * 100) / totalValue) || 0;
+    return positiveValaue;
+  };
+
+  const total = countTotalFeedback();
+  const positiveValaue = countPositiveFeedbackPercentage();
+
+  const optionsObject = { good: good, neutral: neutral, bad: bad };
+  const options = Object.keys(optionsObject);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Layout>
+      <Section title={'Please leave feedback'}>
+        <FeedbackOptions options={options} onLeaveFeedback={updateStatistic} />
+        {total >= 1 ? (
+          <Statistic
+            result={optionsObject}
+            options={options}
+            total={total}
+            positivePercentage={positiveValaue}
+          />
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </Layout>
   );
+};
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+};
+Statistic.propTypes = {
+  result: PropTypes.object.isRequired,
+  options: PropTypes.array.isRequired,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
 };
